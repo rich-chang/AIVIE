363c31a58908d51c15751e251b59e76a1ceb3f62
Rich Chang <rich-chang@outlook.com>
2020-03-30 11:18:12 +0800

    Init MSFT App Center package

--- app/src/main/java/aivie/developer/aivie/MainActivity.java
+++ app/src/main/java/aivie/developer/aivie/MainActivity.java
@@ -3,6 +3,9 @@ package aivie.developer.aivie;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import com.microsoft.appcenter.AppCenter;
+import com.microsoft.appcenter.analytics.Analytics;
+import com.microsoft.appcenter.crashes.Crashes;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        AppCenter.start(getApplication(), "a0aeb136-9ca9-4f08-9502-f81f0d39a301", Analytics.class, Crashes.class);
     }
 }
