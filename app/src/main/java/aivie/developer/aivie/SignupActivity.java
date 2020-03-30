2198c22c011a187bd4a91e49e06ec5507624a4fb
Rich Chang <rich-chang@outlook.com>
2020-03-30 13:18:34 +0800

    Implement basic UI element of Login and Signup activity

--- app/src/main/java/aivie/developer/aivie/SignupActivity.java
+++ app/src/main/java/aivie/developer/aivie/SignupActivity.java
@@ -12,23 +12,23 @@ import android.text.style.UnderlineSpan;
 import android.view.View;
 import android.widget.TextView;
 
-public class LoginActivity extends AppCompatActivity {
+public class SignupActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_signup);
 
-        TextView textView = findViewById(R.id.textViewNeedAccount);
-        SpannableString sp1 = new SpannableString(getResources().getString(R.string.need_account));
-        SpannableString sp2 = new SpannableString(getResources().getString(R.string.need_account_sign_up));
+        TextView textView = findViewById(R.id.textViewHaveAccount);
+        SpannableString sp1 = new SpannableString(getResources().getString(R.string.have_account));
+        SpannableString sp2 = new SpannableString(getResources().getString(R.string.have_account_log_in));
         sp2.setSpan(new ForegroundColorSpan(Color.BLUE), 0, sp2.length(), 0);
         sp2.setSpan(new UnderlineSpan(), 0, sp2.length(), 0);
         textView.setText(TextUtils.concat(sp1, " ", sp2));
     }
 
-    public  void goSignupActivity (View view) {
-        Intent intent = new Intent(getApplicationContext(), SignupActivity.class);
+    public  void goLoginActivity (View view) {
+        Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
         startActivity(intent);
     }
 }
