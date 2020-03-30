a3009233bfd8e1b71dea2e09722a8319411aa560
Rich Chang <rich-chang@outlook.com>
2020-03-30 13:55:36 +0800

    Implement Firebase Auth in Login and Signup activity
    To get basic auth user info.

--- app/src/main/java/aivie/developer/aivie/LoginActivity.java
+++ app/src/main/java/aivie/developer/aivie/LoginActivity.java
@@ -1,5 +1,6 @@
 package aivie.developer.aivie;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -9,22 +10,74 @@ import android.text.SpannableString;
 import android.text.TextUtils;
 import android.text.style.ForegroundColorSpan;
 import android.text.style.UnderlineSpan;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        // Implement customized string
         TextView textView = findViewById(R.id.textViewNeedAccount);
         SpannableString sp1 = new SpannableString(getResources().getString(R.string.need_account));
         SpannableString sp2 = new SpannableString(getResources().getString(R.string.need_account_sign_up));
         sp2.setSpan(new ForegroundColorSpan(Color.BLUE), 0, sp2.length(), 0);
         sp2.setSpan(new UnderlineSpan(), 0, sp2.length(), 0);
         textView.setText(TextUtils.concat(sp1, " ", sp2));
+
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    public void logIn (View view) {
+
+        EditText editTextEmail = findViewById(R.id.username);
+        EditText editTextPassword = findViewById(R.id.password);
+
+        String email = editTextEmail.getText().toString();
+        String password = editTextPassword.getText().toString();
+
+        logInToFirebaseByEmail(email, password);
+    }
+
+    public void logInToFirebaseByEmail (String email, String password) {
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("richc", "signInWithEmail:success");
+
+                            FirebaseUser user = mAuth.getCurrentUser();
+
+                            if (user != null) {
+                                Log.i("richc", "getDisplayName: " + user.getDisplayName());
+                                Log.i("richc", "getPhotoUrl: " + user.getPhotoUrl().toString());
+                                Toast.makeText(LoginActivity.this, "Welcome " + user.getDisplayName(), Toast.LENGTH_LONG).show();
+                            }
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("richc", "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(LoginActivity.this, "Authentication failed.\r\n", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
     }
 
     public  void goSignupActivity (View view) {
--- app/src/main/java/aivie/developer/aivie/SignupActivity.java
+++ app/src/main/java/aivie/developer/aivie/SignupActivity.java
@@ -1,30 +1,97 @@
 package aivie.developer.aivie;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
 import android.text.SpannableString;
 import android.text.TextUtils;
 import android.text.style.ForegroundColorSpan;
 import android.text.style.UnderlineSpan;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
 
 public class SignupActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private String photoUriString = "https://ui-avatars.com/api/?size=128&rounded=true&background=0D8ABC&color=fff&name=";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
 
+        // Implement customized string
         TextView textView = findViewById(R.id.textViewHaveAccount);
         SpannableString sp1 = new SpannableString(getResources().getString(R.string.have_account));
         SpannableString sp2 = new SpannableString(getResources().getString(R.string.have_account_log_in));
         sp2.setSpan(new ForegroundColorSpan(Color.BLUE), 0, sp2.length(), 0);
         sp2.setSpan(new UnderlineSpan(), 0, sp2.length(), 0);
         textView.setText(TextUtils.concat(sp1, " ", sp2));
+
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    public void signUp (View view) {
+
+        EditText editTextDisplayName = findViewById(R.id.displayname);
+        EditText editTextEmail = findViewById(R.id.username);
+        EditText editTextPassword = findViewById(R.id.password);
+
+        String displayName = editTextDisplayName.getText().toString();
+        String email = editTextEmail.getText().toString();
+        String password = editTextPassword.getText().toString();
+
+        logInToFirebaseByEmail(displayName, email, password);
+    }
+
+    public void logInToFirebaseByEmail (final String displayName, String email, String password) {
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("richc", "createUserWithEmail:success");
+
+                            FirebaseUser user = mAuth.getCurrentUser();
+
+                            UserProfileChangeRequest profileUpdate = new UserProfileChangeRequest.Builder()
+                                    .setDisplayName(displayName)
+                                    .setPhotoUri(Uri.parse(photoUriString + displayName))
+                                    .build();
+
+                            user.updateProfile(profileUpdate)
+                                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<Void> task) {
+                                            if (task.isSuccessful()) {
+                                                Log.d("richc", "User profile updated.");
+                                            }
+                                        }
+                                    });
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("richc", "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(SignupActivity.this, "Authentication failed.\r\n", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
     }
 
     public  void goLoginActivity (View view) {
