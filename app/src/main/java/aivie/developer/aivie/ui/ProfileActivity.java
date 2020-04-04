7a2444f134df2ae99c71e815a6af41d97e6ad688
Rich <rich_chang@outlook.com>
2020-04-04 22:18:52 +0800

    Minor fixed

--- app/src/main/java/aivie/developer/aivie/ui/ProfileActivity.java
+++ app/src/main/java/aivie/developer/aivie/ui/ProfileActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import aivie.developer.aivie.R;
+
 public class ProfileActivity extends AppCompatActivity {
 
     @Override
