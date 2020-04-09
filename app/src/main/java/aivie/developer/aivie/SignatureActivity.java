c10e4911e861be1343e883b965847158c30d115a
Rich <rich_chang@outlook.com>
2020-04-09 17:03:31 +0800

    Implement the sign/draw content and clear button

--- app/src/main/java/aivie/developer/aivie/SignatureActivity.java
+++ app/src/main/java/aivie/developer/aivie/SignatureActivity.java
@@ -3,12 +3,57 @@ package aivie.developer.aivie;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.github.gcacace.signaturepad.views.SignaturePad;
 
 public class SignatureActivity extends AppCompatActivity {
+
+    private SignaturePad mSignaturePad;
+    private Button mClearButton;
+    private Button mConfirmButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signature);
+
+        mClearButton = findViewById(R.id.clear_button);
+        mConfirmButton = findViewById(R.id.confirm_signed_button);
+        mSignaturePad = findViewById(R.id.signature_pad);
+
+        mSignaturePad.setOnSignedListener(new SignaturePad.OnSignedListener() {
+            @Override
+            public void onStartSigning() {
+                //Toast.makeText(MainActivity.this, "OnStartSigning", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onSigned() {
+                mConfirmButton.setEnabled(true);
+                mClearButton.setEnabled(true);
+            }
+
+            @Override
+            public void onClear() {
+                mConfirmButton.setEnabled(false);
+                mClearButton.setEnabled(false);
+            }
+        });
+
+        mClearButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mSignaturePad.clear();
+            }
+        });
+
+        mConfirmButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Press CONFIRM
+            }
+        });
     }
 }
