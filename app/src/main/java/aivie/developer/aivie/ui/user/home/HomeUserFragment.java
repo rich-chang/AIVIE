ef9fcf64de60f558d0539e1863ba7fc8c755d496
Rich <rich_chang@outlook.com>
2020-04-11 20:05:19 +0800

    Implement scheduled notification with sample delay time

--- app/src/main/java/aivie/developer/aivie/ui/user/home/HomeUserFragment.java
+++ app/src/main/java/aivie/developer/aivie/ui/user/home/HomeUserFragment.java
@@ -1,8 +1,13 @@
 package aivie.developer.aivie.ui.user.home;
 
 import android.app.Activity;
+import android.app.AlarmManager;
+import android.app.Notification;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -33,9 +38,11 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import aivie.developer.aivie.HomeAdmActivity;
 import aivie.developer.aivie.util.Constant;
 import aivie.developer.aivie.LoginActivity;
 import aivie.developer.aivie.R;
+import aivie.developer.aivie.util.NotificationPublisher;
 
 public class HomeUserFragment extends Fragment {
 
@@ -149,6 +156,10 @@ public class HomeUserFragment extends Fragment {
                                     }
 
                                     UpdateUI();
+
+                                    /// Schedule notification for visit reminder
+                                    scheduleNotification(getNotification("Reminder: Visit your doctor on 05/20"), 18000);
+
                                 } else {
                                     if(Constant.DEBUG) Log.d(Constant.TAG, "task get failed with ", task.getException());
                                 }
@@ -205,5 +216,87 @@ public class HomeUserFragment extends Fragment {
                 break;
         }
         return true;
+    }
+
+    private void scheduleNotification(Notification notification, int delay) {
+
+        long futureInMillis = SystemClock.elapsedRealtime() + delay;
+
+        Intent notificationIntent = new Intent(getActivity(), NotificationPublisher.class);
+        notificationIntent.putExtra(NotificationPublisher.NOTIFICATION_ID, 1);
+        notificationIntent.putExtra(NotificationPublisher.NOTIFICATION, notification);
+
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(
+                getActivity(),
+                0,
+                notificationIntent,
+                PendingIntent.FLAG_UPDATE_CURRENT);
+
+        AlarmManager alarmManager = (AlarmManager)getActivity().getSystemService(Context.ALARM_SERVICE);
+        alarmManager.set(AlarmManager.ELAPSED_REALTIME_WAKEUP, futureInMillis, pendingIntent);
+    }
+
+    private Notification getNotification(String content) {
+
+        Notification.Builder builder = new Notification.Builder(getActivity());
+
+        builder.setContentTitle(getString(R.string.app_name));
+        builder.setContentText(content);
+        builder.setSmallIcon(R.drawable.ic_launcher_foreground);
+        builder.setDefaults(Notification.DEFAULT_ALL);
+
+        return builder.build();
+
+        /*
+        //Step3. 透過 Notification.Builder 來建構 notification，
+                //並直接使用其.build() 的方法將設定好屬性的 Builder 轉換
+                //成 notification，最後開始將顯示通知訊息發送至狀態列上。
+                Notification notification
+                   = new Notification.Builder(MainActivity.this)
+                   .setContentIntent(appIntent)
+                   .setSmallIcon(R.drawable.ic_launcher) // 設置狀態列裡面的圖示（小圖示）　　
+                   .setLargeIcon(BitmapFactory.decodeResource(MainActivity.this.getResources(), R.drawable.ic_launcher)) // 下拉下拉清單裡面的圖示（大圖示）
+                   .setTicker("notification on status bar.") // 設置狀態列的顯示的資訊
+                   .setWhen(System.currentTimeMillis())// 設置時間發生時間
+                   .setAutoCancel(false) // 設置通知被使用者點擊後是否清除  //notification.flags = Notification.FLAG_AUTO_CANCEL;
+                   .setContentTitle("Notification Title") // 設置下拉清單裡的標題
+                   .setContentText("Notification Content")// 設置上下文內容
+                   .setOngoing(true)      //true使notification變為ongoing，用戶不能手動清除// notification.flags = Notification.FLAG_ONGOING_EVENT; notification.flags = Notification.FLAG_NO_CLEAR;
+                   .setDefaults(Notification.DEFAULT_ALL) //使用所有默認值，比如聲音，震動，閃屏等等
+//                 .setDefaults(Notification.DEFAULT_VIBRATE) //使用默認手機震動提示
+//                 .setDefaults(Notification.DEFAULT_SOUND) //使用默認聲音提示
+//                 .setDefaults(Notification.DEFAULT_LIGHTS) //使用默認閃光提示
+//                 .setDefaults(Notification.DEFAULT_LIGHTS | Notification.DEFAULT_SOUND) //使用默認閃光提示 與 默認聲音提示
+
+                   .setVibrate(vibrate) //自訂震動長度
+//                 .setSound(uri) //自訂鈴聲
+//                 .setLights(0xff00ff00, 300, 1000) //自訂燈光閃爍 (ledARGB, ledOnMS, ledOffMS)
+
+                   .build();
+
+                // 將此通知放到通知欄的"Ongoing"即"正在運行"組中
+                notification.flags = Notification.FLAG_ONGOING_EVENT;
+
+                // 表明在點擊了通知欄中的"清除通知"後，此通知不清除，
+                // 經常與FLAG_ONGOING_EVENT一起使用
+                notification.flags = Notification.FLAG_NO_CLEAR;
+
+                //閃爍燈光
+                notification.flags = Notification.FLAG_SHOW_LIGHTS;
+
+                // 重複的聲響,直到用戶響應。
+                notification.flags = Notification.FLAG_INSISTENT; /
+
+
+                // 把指定ID的通知持久的發送到狀態條上.
+                mNotificationManager.notify(0, notification);
+
+                // 取消以前顯示的一個指定ID的通知.假如是一個短暫的通知，
+                // 試圖將之隱藏，假如是一個持久的通知，將之從狀態列中移走.
+//              mNotificationManager.cancel(0);
+
+                //取消以前顯示的所有通知.
+//              mNotificationManager.cancelAll();
+        */
     }
 }
--- app/src/main/java/aivie/developer/aivie/util/NotificationPublisher.java
+++ app/src/main/java/aivie/developer/aivie/util/NotificationPublisher.java
@@ -14,11 +14,9 @@ public class NotificationPublisher extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
 
         NotificationManager notificationManager = (NotificationManager)context.getSystemService(Context.NOTIFICATION_SERVICE);
-
         Notification notification = intent.getParcelableExtra(NOTIFICATION);
         int id = intent.getIntExtra(NOTIFICATION_ID, 0);
         notificationManager.notify(id, notification);
-
     }
 
 }
