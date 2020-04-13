73fd61e7721e03572cd750533a3ec40375e12cf1
Rich <rich_chang@outlook.com>
2020-04-13 15:00:25 +0800

    Init listView in Adm management fragment

--- app/src/main/java/aivie/developer/aivie/ui/adm/management/ManagementFragment.java
+++ app/src/main/java/aivie/developer/aivie/ui/adm/management/ManagementFragment.java
@@ -1,9 +1,13 @@
 package aivie.developer.aivie.ui.adm.management;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -12,11 +16,30 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.Objects;
+
+import aivie.developer.aivie.HomeAdmActivity;
 import aivie.developer.aivie.R;
+
+import static aivie.developer.aivie.util.Constant.DEBUG;
+import static aivie.developer.aivie.util.Constant.TAG;
 
 public class ManagementFragment extends Fragment {
 
     private ManagementViewModel managementViewModel;
+    private FirebaseFirestore db;
+    private FirebaseAuth mAuth;
+    ListView ptListView;
+    static ArrayList<String> patients = new ArrayList<>();
+    static ArrayAdapter<String> arrayAdapter;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -30,6 +53,39 @@ public class ManagementFragment extends Fragment {
                 textView.setText(s);
             }
         });
+
+        db = FirebaseFirestore.getInstance();
+        mAuth = FirebaseAuth.getInstance();
+
+        ptListView = root.findViewById(R.id.ptListView);
+        arrayAdapter = new ArrayAdapter<>((HomeAdmActivity) Objects.requireNonNull(getActivity()), android.R.layout.simple_list_item_1, patients);
+        ptListView.setAdapter(arrayAdapter);
+        ptListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            }
+        });
+
+        refresh();
+
         return root;
+    }
+
+    private void refresh() {
+
+        db.collection(getString(R.string.firestore_users))
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                if (DEBUG) Log.d(TAG, document.getId() + " => " + document.getData());
+                            }
+                        } else {
+                            if (DEBUG) Log.d(TAG, "Error getting documents: ", task.getException());
+                        }
+                    }
+                });
     }
 }
