83b63c0e6e4955194bdbbf489f30330e76174057
Rich <rich_chang@outlook.com>
2020-04-13 16:09:22 +0800

    Implement display patient list in viewFrag

--- app/src/main/java/aivie/developer/aivie/ui/adm/management/ManagementFragment.java
+++ app/src/main/java/aivie/developer/aivie/ui/adm/management/ManagementFragment.java
@@ -8,17 +8,16 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
+import androidx.fragment.app.ListFragment;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
@@ -26,18 +25,18 @@ import com.google.firebase.firestore.QuerySnapshot;
 import java.util.ArrayList;
 import java.util.Objects;
 
-import aivie.developer.aivie.HomeAdmActivity;
 import aivie.developer.aivie.R;
+import aivie.developer.aivie.util.Constant;
 
 import static aivie.developer.aivie.util.Constant.DEBUG;
 import static aivie.developer.aivie.util.Constant.TAG;
 
-public class ManagementFragment extends Fragment {
+public class ManagementFragment extends ListFragment {
 
     private ManagementViewModel managementViewModel;
     private FirebaseFirestore db;
     private FirebaseAuth mAuth;
-    ListView ptListView;
+    static ArrayList<String> patientsList = new ArrayList<>();
     static ArrayList<String> patients = new ArrayList<>();
     static ArrayAdapter<String> arrayAdapter;
 
@@ -46,29 +45,22 @@ public class ManagementFragment extends Fragment {
         managementViewModel =
                 ViewModelProviders.of(this).get(ManagementViewModel.class);
         View root = inflater.inflate(R.layout.fragment_management_adm, container, false);
-        final TextView textView = root.findViewById(R.id.text_gallery);
-        managementViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+
 
         db = FirebaseFirestore.getInstance();
         mAuth = FirebaseAuth.getInstance();
 
-        ptListView = root.findViewById(R.id.ptListView);
-        arrayAdapter = new ArrayAdapter<>((HomeAdmActivity) Objects.requireNonNull(getActivity()), android.R.layout.simple_list_item_1, patients);
-        ptListView.setAdapter(arrayAdapter);
-        ptListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-            }
-        });
+        arrayAdapter = new ArrayAdapter<>(Objects.requireNonNull(getActivity()), android.R.layout.simple_list_item_1, patientsList);
+        setListAdapter(arrayAdapter);
 
         refresh();
 
         return root;
+    }
+
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id) {
+        // TODO implement some logic
     }
 
     private void refresh() {
@@ -79,8 +71,36 @@ public class ManagementFragment extends Fragment {
                     @Override
                     public void onComplete(@NonNull Task<QuerySnapshot> task) {
                         if (task.isSuccessful()) {
-                            for (QueryDocumentSnapshot document : task.getResult()) {
-                                if (DEBUG) Log.d(TAG, document.getId() + " => " + document.getData());
+
+                            for (QueryDocumentSnapshot documentUser : task.getResult()) {
+
+                                if (DEBUG) Log.d(TAG, documentUser.getId() + " => " + documentUser.getData());
+                                final String lastName = (String) documentUser.get(getString(R.string.firestore_users_last_name));
+                                final String firstName = (String) documentUser.get(getString(R.string.firestore_users_first_name));
+                                final String userId = (String) documentUser.getId();
+
+                                DocumentReference docRefRole = (DocumentReference) documentUser.get(getString(R.string.firestore_users_role));
+                                docRefRole.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                                        if (task.isSuccessful()) {
+
+                                            DocumentSnapshot documentRole = task.getResult();
+                                            if ((documentRole
+                                                    .get(getString(R.string.firestore_column_id)))
+                                                    .equals(getString(R.string.firestore_role_pt))) {
+
+                                                patients.add(userId);
+                                                patientsList.add(firstName + " " + lastName);
+                                                 arrayAdapter.notifyDataSetChanged();
+                                                 //arrayAdapter.notifyDataSetInvalidated();
+                                             }
+
+                                        } else {
+                                            if(Constant.DEBUG) Log.d(Constant.TAG, "task get failed with ", task.getException());
+                                        }
+                                    }
+                                });
                             }
                         } else {
                             if (DEBUG) Log.d(TAG, "Error getting documents: ", task.getException());
